aeab6dc4d709e7a1d945a2b5c7d51fd6ed65f5c0
Felipe Fioreti Rosolen <0040482411006@americana.fatec.br>
2025-03-12 09:19:44 -0300

    Adicionando HappyNumber NÃO FINALIZADO

---
+++ HappyNumber/HappyNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿
+bool isHappy(int n)
+{
+    int number = n;
+
+    int count = 0;
+    int agregate = 0;
+
+    LinkedList<int> digitsNumber = new LinkedList<int>();
+    LinkedList<int> numberList = new LinkedList<int>();
+
+    while (true)
+    {
+        while (number > 0)
+        {
+            digitsNumber.AddFirst(number % 10);
+
+            number /= 10;
+        }
+
+        foreach (var item in digitsNumber)
+        {
+            agregate += item * item;
+        }
+
+        Console.WriteLine(agregate);
+
+        if (agregate == 1)
+        {
+            return false;
+        }
+
+        if (numberList.Contains(agregate))
+        {
+            return false;
+        }
+
+        numberList.AddLast(agregate);
+        number = agregate;
+        agregate = 0;
+    }
+}
+
+isHappy(19);
