30d063b1072f40b6c9d9da9f8816b01fd9edb15f
Felipe Fioreti <felipe.fioretirosolen@gmail.com>
2026-06-13 02:26:19 -0300

    Add MapWordWeights

---
+++ Weighted Word Mapping/Weighted Word Mapping/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(MapWordWeights(["abcd","def","xyz"], [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]));
+
+string MapWordWeights(string[] words, int[] weights) {
+        string res = "";
+        int sum = 0;
+        foreach(var word in words){
+            foreach(var letter in word){
+                sum += weights[letter - 'a'];
+            }
+            res += (char)((int)'z' - (sum % 26));
+            sum = 0;
+        }
+
+        return res;
+}
